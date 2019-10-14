6d9af71ccecfe9030d31b882a8f10dd1b7d4e0ff
ticeedu <55580053+ticeedu@users.noreply.github.com>
2019-10-14 16:35:28 +0200

    Add files via upload

---
+++ genereListeEleveMagret.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+nom = "ELEV"
+prenom = "ELEVE"
+classe = "999"
+
+borneInf = 32
+borneSup = 64
+
+for x in range(borneInf,borneSup):
+	print  nom + str(x) + "," +  prenom  + str(x) + "," + classe
+
+
+
